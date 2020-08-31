9ad355c6c98fc9fafa379e1f0255a177184e49b5
Zwee Dao <zweedao@gmail.com>
2020-08-31 13:33:36 -0400

    fix

--- scripts/find_trash.py
+++ scripts/find_trash.py
@@ -18,12 +18,12 @@ detect_publisher = rospy.Publisher('state',String,queue_size=1)
 
 #camera conversion: 1m = 1650px
 #image resolution: 640px * 480px
-CAMERA_METER_TO_PIXEL = 500 #2000
-IMAGE_HEIGTH = 240 #480
-IMAGE_WIDTH = 320 #640
+CAMERA_METER_TO_PIXEL = 2000 #500 #2000
+IMAGE_HEIGTH = 480 #240 #480
+IMAGE_WIDTH = 320 #320 #640
 IMAGE_CENTER_X = IMAGE_WIDTH / 2
-TRASH_CONST = 50.0 #99.0 #trash_distance_meter * trash_image_height_pixel
-TRASH_MAX_WIDTH = 100 #200
+TRASH_CONST = 99.0 #50.0 #99.0 #trash_distance_meter * trash_image_height_pixel
+TRASH_MAX_WIDTH = 200 #100 #200
 
 
 def odom_callback(data):
