e475a6d8bac844e120c654901264d8ddbe70b021
Zwee Dao <zweedao@gmail.com>
2020-08-31 13:24:36 -0400

    fix

--- scripts/find_trashcan.py
+++ scripts/find_trashcan.py
@@ -55,8 +55,8 @@ def find_trashcan():
     trashcan_q = quaternion_from_euler(roll, pitch, yaw)
     
     # set goal in front of trashcan, instead of at the trash can itself
-    goal_x = TRASHCAN_POSE[0] - 0.40*cos(yaw)
-    goal_y = TRASHCAN_POSE[1] - 0.40*sin(yaw)
+    goal_x = TRASHCAN_POSE[0] - 0.41*cos(yaw)
+    goal_y = TRASHCAN_POSE[1] - 0.41*sin(yaw)
     print('goal_x', goal_x)
     print('goal_y', goal_y)
 
